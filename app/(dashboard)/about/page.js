c30c97d6410535216b0682e93b07597345bbdba5
benjimosso <benjimosso@gmail.com>
2024-02-09 15:23:27 -0800

    about

--- app/(dashboard)/about/page.js
+++ app/(dashboard)/about/page.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Archivo_Narrow } from 'next/font/google'
 
 export default function page() {
   return (
-    <div className='flex-1'>
-      <h1>this is the about page. </h1>
+    <div className='flex flex-1 justify-center'>
+      <h1 className='font-bold text-cyan-400 text-3xl pt-20'>About this Website</h1>
     </div>
   )
 }
