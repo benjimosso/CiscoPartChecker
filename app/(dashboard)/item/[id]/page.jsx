d5846559cd0e76fda4c645bffe1a47efd62e2f47
benjimosso <benjimosso@gmail.com>
2024-03-28 15:03:33 -0700

    a

--- app/(dashboard)/item/[id]/page.jsx
+++ app/(dashboard)/item/[id]/page.jsx
@@ -32,8 +32,9 @@ export default async function SingleItemShow({ params }) {
   // console output of a many to many relationship between cisco and ciscofans and fans
   // console.log(single.ciscopowers);
   // here the 0 next to ciscofans is the index of the array, in case is more than one i can map through it... example below, it can be item or item.fans or item.fans.fan_pn
- single.ciscofans.map((item) => console.log(item.fans.fan_pn));
- single.ciscopowers.map((item) => console.log(item.powers.power_pn));
+//  single.ciscofans.map((item) => console.log(item.fans.fan_pn));
+//  single.ciscopowers.map((item) => console.log(item.powers.power_pn));
+console.log(single)
   
 
   if (error) {
