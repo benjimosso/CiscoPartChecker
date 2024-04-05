e4c596bb21dd74e45e4fb71a30bfbe510ca4d070
benjimosso <benjimosso@gmail.com>
2024-04-05 12:56:43 -0700

    Home page V1 message

--- app/(dashboard)/page.jsx
+++ app/(dashboard)/page.jsx
@@ -1,11 +1,13 @@
-
-
 export default function Home() {
- 
-
   return (
-    <main className="flex-1">
-     
+    <main className="flex-1 ">
+      <div className=" mt-28 ">
+        <div className="flex flex-col gap-10 justify-center items-center">
+      <h1 className="text-pretty text-2xl font-bold">Welcome to the Cisco Part Checker Version 1</h1>
+      <h2 className="text-xl font-semibold">Please notice this is a site in progress, Thank you</h2>
+      <p className="font-semibold">Daniel</p>
+      </div>
+      </div>
     </main>
   );
 }
